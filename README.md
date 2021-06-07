7c0fc07c96f2e54396ba75cae948b5c3e78235f6
Nandhabalan Marimuthu <67896802+nandha-marimuthu@users.noreply.github.com>
2021-06-07 10:52:53 +0530

    base changes

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # covac
 Vaccination portal
+
+# creating a branch
+
+git branch branchname
+git checkout branchname
+
+# pushing
+
+git add .
+git commit -m 'commit message'
+git push
---
+++ home.py
@@ -0,0 +1,18 @@
+from pandas.core.frame import DataFrame
+import streamlit as st
+import numpy as np
+import pandas as pd
+from pymongo import MongoClient
+
+#connecting with the mongodb cluster using the connection token
+client = MongoClient('mongodb+srv://dbuser1:1234@eshop.m8tu7.mongodb.net/test')
+
+#declaring the database
+db = client['Covac']
+
+#declaring the collections
+c1 = db['centers']
+c2 = db['admin']
+c1 = db['patient']
+c1 = db['appoinment']
+c1 = db['vaccinated']
